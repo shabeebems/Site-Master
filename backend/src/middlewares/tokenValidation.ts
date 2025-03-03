5302b8098934d3394b758763beb35d0a0909c1cc
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2025-03-03 11:14:07 +0530

    structure

--- backend/src/middlewares/tokenValidation.ts
+++ backend/src/middlewares/tokenValidation.ts
@@ -9,14 +9,38 @@ export const authenticateToken = async(
 ) => {
     try {
         const accessToken = req.cookies.accessToken
-        if(accessToken) {
-            console.log('Access')
+        if(!accessToken) {
+
+            console.log('No access token, waiting for creation with refresh token')
+
+            const refreshToken = req.cookies.refreshToken
+            if(!refreshToken) {
+                console.log('No refresh, logout')
+            } else {
+                jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string, (err: any, decoded: any) => {
+                    if(err) {
+                        console.log('No valid refresh token, Exit')
+                    } else {
+                        console.log('Refresh token valid, creating new access token.')
+                        next()
+                    }
+                })
+            }
+            next()
+
+        } else {
+            
+            jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET as string, (err: any, decoded: any) => {
+                if(err) {
+                    console.log('No valid access token, Exit')
+                } else {
+                    console.log('Valid access, Success go next')
+                    next()
+                }
+            })
+            
         }
-        const refreshToken = req.cookies.refreshToken
-        if(refreshToken) {
-            console.log('refresh')
-        }
-        next()
+
     } catch (error) {
         console.log(error)
     }
