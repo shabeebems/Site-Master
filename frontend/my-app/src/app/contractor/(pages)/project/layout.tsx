ebe9c23c9f468735c05c06911caff5f1f7041071
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2025-03-17 13:52:56 +0530

    UI single project

--- frontend/my-app/src/app/contractor/(pages)/project/layout.tsx
+++ frontend/my-app/src/app/contractor/(pages)/project/layout.tsx
@@ -9,15 +9,15 @@ export default function RootLayout({
   const active: string = 'Project'
     
     return (
-      <>
       <div className='flex bg-blue-50'>
         <Sidebar active={active} />
-        <div className='flex-1 max-h-screen'>
-          <Navbar active={active} />
-          {children}
+        <div className='flex-1 flex flex-col max-h-screen overflow-auto bg-blue-50'>
+          <div className="sticky top-0 z-50 bg-blue-50 shadow-md">
+            <Navbar active={active} />
+          </div>
+          <div className="p-6 pb-12">{children}</div>
         </div>
       </div>
-      </>
 )
     
 }
