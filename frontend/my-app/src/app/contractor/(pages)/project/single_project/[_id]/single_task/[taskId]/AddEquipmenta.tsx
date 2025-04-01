559cf2a1fd592221d446012d2a477a9e4c106c14
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2025-04-01 20:58:48 +0530

    Worker Management

--- frontend/my-app/src/app/contractor/(pages)/project/single_project/[_id]/single_task/[taskId]/AddEquipmenta.tsx
+++ frontend/my-app/src/app/contractor/(pages)/project/single_project/[_id]/single_task/[taskId]/AddEquipmenta.tsx
@@ -110,7 +110,6 @@ const AddEquipment: React.FC<ProjectModalProps> = ({ equipmentAdditionSuccess, c
             if(response.success) {
                 toast.success(response.message, { position: "top-right", });
                 setInputs([])
-                
                 equipmentAdditionSuccess(inputs)
             } else {
                 toast.error(response.message, { position: "top-right", });
